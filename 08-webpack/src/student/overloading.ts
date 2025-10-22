82c61a2196427f9779f500601a1f140a0184106f
Kevin Cunningham <kevin@kevincunningham.co.uk>
2025-10-22 11:28:06 +0100

    adds no handling

--- 08-webpack/src/student/overloading.ts
+++ 08-webpack/src/student/overloading.ts
@@ -1,12 +1,37 @@
 // Step 1: Define the overload signatures for the getValue function
 
 // Your overload signatures here
+// function getValue(targetString: string): string;
+// function getValue(targetNumber: number): number;
+// function getValue(obj: boolean): boolean;
+function getValue<T>(obj: T): T;
+function getValue<T>(obj: T[], index: number): T;
+function getValue<T extends object, K extends keyof T>(obj: T, key: K): T[K];
 
 // Step 2: Implement the getValue function to handle the different cases
-function getValue(obj: any, index?: number): any {}
+function getValue(obj: any, index?: number): any {
+  if (Array.isArray(obj) && typeof index === "number") {
+    return obj[index];
+  } else if (
+    typeof obj === "object" &&
+    obj != null &&
+    typeof index === "string"
+  ) {
+    return obj[index];
+  }
+  return obj;
+}
 
 // Step 3: Test Cases
 
 const strVal = getValue("hello"); // Should return "hello"
 const numVal = getValue(42); // Should return 42
-const arrVal = getValue([10, 20, 30], 1); // Should return 20
+const arrVal = getValue([10, 20, 30, "40"], 1); // Should return 20
+const boolVal = getValue(true);
+const objVal = getValue({ name: 42, location: "Belfast" }, "name");
+
+console.log(strVal);
+console.log(numVal);
+console.log(arrVal);
+console.log(boolVal);
+console.log(objVal);
---
+++ 09-errors/src/01-no-error-handling.ts
@@ -0,0 +1,23 @@
+import readline from "node:readline/promises";
+
+async function ask(): Promise<string> {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const answer = await rl.question("When is your birthday?");
+  rl.close();
+  return answer || "";
+}
+
+function parse(birthday: string) {
+  return new Date(birthday);
+}
+
+async function main() {
+  const input = await ask();
+  const date = parse(input);
+  console.log(`Your birthday is ${date.toISOString()}`);
+}
+
+main();
